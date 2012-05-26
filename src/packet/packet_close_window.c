40921dd302665b57c253e9e1b6b5642fd745d3d2
Adam <Adam@anope.org>
2012-05-26 15:36:24 -0400

    Oops

--- src/packet/packet_close_window.c
+++ src/packet/packet_close_window.c
@@ -3,7 +3,7 @@
 
 int packet_close_window(struct bedrock_client *client, const unsigned char *buffer, size_t len)
 {
-	size_t offset;
+	size_t offset = 1;
 	uint8_t window;
 
 	packet_read_int(buffer, len, &offset, &window, sizeof(window));
