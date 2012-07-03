a39391dbdd26b3c6aaa4ffa2a4a7f50e4982d175
Adam <Adam@anope.org>
2012-07-02 21:47:19 -0400

    Change /version output to be more like -version

--- src/command/command_version.c
+++ src/command/command_version.c
@@ -3,5 +3,5 @@
 
 void command_version(struct bedrock_client *client, int bedrock_attribute_unused argc, const char bedrock_attribute_unused **argv)
 {
-	command_reply(client, "Bedrock version %d.%d%s Compiled on %s %s.", BEDROCK_VERSION_MAJOR, BEDROCK_VERSION_MINOR, BEDROCK_VERSION_EXTRA, __DATE__, __TIME__);
+	command_reply(client, "Bedrock version %d.%d%s, built on %s %s.", BEDROCK_VERSION_MAJOR, BEDROCK_VERSION_MINOR, BEDROCK_VERSION_EXTRA, __DATE__, __TIME__);
 }
