235978f6158b2b0b19b811f7c64460c4eab357ce
Adam <Adam@anope.org>
2012-08-06 05:00:48 -0400

    New column stuff

--- include/packet/packet_column.h
+++ include/packet/packet_column.h
@@ -1,3 +1,2 @@
 
-extern void packet_send_column(struct bedrock_client *client, struct bedrock_column *column);
 extern void packet_send_column_empty(struct bedrock_client *client, struct bedrock_column *column);
